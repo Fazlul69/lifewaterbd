d63bfe18ad577ba4124ed894ab3be437bdffb0d1
Fazlul Hoque <sawravm299@gmail.com>
2020-09-14 18:43:52 +0600

    Initial commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,66 @@
+<style type="text/css">
+	.bg-light {background-color: #4D80D0!important;}
+    a.navbar-brand {margin-left: 250px;}
+	img.logo {border-radius: 40px;}
+	ul>li>a:hover{text-decoration:none;}
+	#dLabel,ul>li>a{color: #fff;}
+	ul.navbar-nav {
+    margin-right: 350px;}
+	.home_tag {
+    margin-right: 20px;}
+	.life_tag {
+    margin-left: 20px;}
+    ul.dropdown-menu.drop_all.show {
+    width: 275px;
+    background-color: #fff;
+}
+.drop_list {
+    margin-left: 10px;
+    text-transform: uppercase;
+    color: #000;
+}
+.drop_list:hover {
+    color: #FFF;
+    text-decoration: none;
+    background-color: #4d80d0;
+}
+
+</style>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a class="navbar-brand" href="index.html"><img src="img/logo.jpg" class="logo"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+           
+        </ul> 
+        <ul class="navbar-nav">
+            <li class="nav-item home_tag">
+                <a href="index.html" class="active">HOME <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item ">
+                <div class="dropdown">
+                        <a id="dLabel" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            PRODUCTS
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu drop_all" aria-labelledby="dLabel">
+                            <li><a href="products.html" class="drop_list">Reverse osmosis system</a></li>
+                            <li><a href="under_counter.html" class="drop_list">Under counter System</a></li>
+                            <li><a href="single_filter.html" class="drop_list">Single Filter System</a></li>
+                            <li><a href="accessories.html" class="drop_list">Filter accessories</a></li>
+                            <li><a href="industrial.html" class="drop_list">Industrial Water Treatment</a></li>
+                            <li><a href="industrial.html" class="drop_list">Water Dispenser</a></li>
+                            <li><a href="techno.html" class="drop_list">Technologies</a></li>
+                        </ul>
+                    </div>
+            </li>
+             <li class="nav-item life_tag">
+                <a href="life_water.html">LIFE WATER</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+		
---
+++ resources/views/slider.blade.php
@@ -0,0 +1,44 @@
+
+<style type="text/css">
+	.b_text{
+    font-size: 60px;
+    color: #d8c80c;
+    margin-bottom: 70px;}
+</style>
+
+
+<div class="container">
+	<div class="row">
+		 <div class="col align-self-center">
+		 	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			  <ol class="carousel-indicators">
+			    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			  </ol>
+			  <div class="carousel-inner">
+			  	<div class="carousel-caption">
+			            <p class="b_text text-center
+			                     ">Change The Way <br>You Look At Water</p>
+			        </div>
+			    <div class="carousel-item active">
+			      <img src="img/carousel-1.jpg" class="d-block w-100" alt="...">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="img/carousel-2.png" class="d-block w-100" alt="...">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="img/carousel-3.jpg" class="d-block w-100" alt="...">
+			    </div>
+			  </div>
+            </div>
+		 </div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+	$('.carousel').carousel({
+  interval: 5000
+})
+</script>
