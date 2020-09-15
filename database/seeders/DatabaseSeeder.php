757198d108bfd730a0c8829492f8f0608456ceae
Fazlul Hoque <sawravm299@gmail.com>
2020-09-15 12:27:26 +0600

    admin login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        /*Model::unguard();
+        $this->call(UsersTablesSeeder::class);*/
+        DB::table('users')->insert([
+            'name' => str_random(10).'Faysal',
+            'email' => str_random(10).'lifewaterbd@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+    public function run(){
+    	$this->call(UsersTablesSeeder::class);
     }
 }
---
+++ database/seeders/UsersTablesSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*DB::table('users')->insert([
+        	'name'				=> 'Faysal',
+        	'email'				=> 'lifewaterbd@gmail.com',
+        	'password'			=> Hash::make('lifewater99'),
+        	'remember_token'	=> str_random(10)
+        ]);*/
+    }
+}
